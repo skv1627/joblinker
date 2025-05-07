6e975f5953b26ed9e4cc078346bebee2c782e718
Swarna Kumar Vusa <swarnakumarvusa@gmail.com>
2025-05-06 20:42:47 -0400

    fix: add Spring Boot main class

---
+++ backend/src/main/java/com/joblinker/JoblinkerApplication.java
@@ -0,0 +1,12 @@
+package com.joblinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblinkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoblinkerApplication.class, args);
+    }
+}
